d49859eb35e15516580b6ddf93d2101ef21fd6e8
Joseph <josephchai100@hotmail.com>
2024-05-15 01:31:31 +0100

    surround.nvim

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,11 +1,24 @@
 return {
-      {
-        "kylechui/nvim-surround",
-        -- tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-          require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-          })
-        end,
+  {
+    "kylechui/nvim-surround",
+    -- tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup {
+        surrounds = {
+          ["("] = {
+            add = function() return { { "(" }, { ")" } } end,
+          },
+          ["{"] = {
+            add = function() return { { "{" }, { "}" } } end,
+          },
+          ["["] = {
+            add = function() return { { "[" }, { "]" } } end,
+          },
+          ["<"] = {
+            add = function() return { { "<" }, { ">" } } end,
+          },
+        },
+      }
+    end,
   },
 }
