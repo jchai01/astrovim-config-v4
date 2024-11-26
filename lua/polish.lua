6cf52337ae774825894861986405ea8f1500d0bb
Joseph <josephchai100@hotmail.com>
2024-11-26 19:44:38 +0000

    .

--- lua/polish.lua
+++ lua/polish.lua
@@ -23,3 +23,7 @@ vim.opt.whichwrap:append "<,>,h,l,[,]"
 -- x for only visual mode
 -- reason: https://superuser.com/questions/321547/how-do-i-replace-paste-yanked-text-in-vim-without-yanking-the-deleted-lines
 vim.keymap.set("x", "p", "P")
+
+-- easier whole word delete while in insert mode
+vim.keymap.set("i", "<C-Del>", "<Esc>lce")
+vim.keymap.set("i", "<C-H>", "<C-W>") -- <C-H> is the same as ctrl-backspace, <C-BS>
