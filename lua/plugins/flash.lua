7465462be11ba3748c5d96cb425b592742a276c5
Joseph <josephchai100@hotmail.com>
2024-12-02 03:10:07 +0000

    .

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,14 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    -- prefer the vanilla f/t feature
+    modes = {
+      char = {
+        enabled = false,
+      },
+    },
+  },
   -- stylua: ignore
   keys = {
     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
